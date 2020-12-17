3e026c9147970bda4590076ced1455aa8a6b02e0
MaiQD <q.dat2710@gmail.com>
2020-12-18 01:14:54 +0700

    Add AppUser Controller

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,32 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+	[ApiController]
+	[Route("api/[controller]")]
+	public class UsersController : ControllerBase
+	{
+		private readonly DataContext _dataContext;
+
+		public UsersController(DataContext dataContext)
+		{
+			_dataContext = dataContext;
+		}
+		[HttpGet]
+		public ActionResult<IEnumerable<AppUser>> GetUsers()
+		{
+			return _dataContext.AppUsers.ToList();
+		}
+		[HttpGet("{id}")]
+		public ActionResult<AppUser> GetUsers(int id)
+		{
+			return _dataContext.AppUsers.Find(id);
+		}
+	}
+}
--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 dotnet new sln
 donet new webapi -o API
 dotnet sln add API
+
+--thêm mới migrations
+add-migration InitialDB -o Data/Migrations
+
+Update-Database
